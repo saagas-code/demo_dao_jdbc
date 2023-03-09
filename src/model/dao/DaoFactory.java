6ba86f22f83140dadab28d0527caf91e1b6a687c
saagas-code <saagas.code@gmail.com>
2023-03-09 19:08:59 -0300

    save the files

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,9 @@
 package model.dao;
 
-public class DaoFactory {
+import model.dao.impl.SellerDaoJDBC;
 
+public class DaoFactory {
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC();
+	}
 }
