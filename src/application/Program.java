934d3777f85db7a00894c11c88f8beb95eda5cb8
saagas-code <saagas.code@gmail.com>
2023-03-11 08:54:35 -0300

    add delete implement

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -32,16 +34,24 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("=== TESTE 3: seller insert ====");
+		System.out.println("=== TESTE 4: seller insert ====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", LocalDate.now(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
-		System.out.println("=== TESTE 4: seller UPDATE ====");
+		System.out.println("=== TESTE 5: seller UPDATE ====");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
+		
+		System.out.println("=== TESTE 6: seller DELETE ====");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
 	}
 
 }
